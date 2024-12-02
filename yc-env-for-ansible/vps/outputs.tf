a8b700d7ca564fa0b0fdac21e92dc3644f54fff2
gaidar.vu <gaidar.vu@gmail.com>
2024-12-02 13:01:46 +0300

    ans3

--- yc-env-for-ansible/vps/outputs.tf
+++ yc-env-for-ansible/vps/outputs.tf
@@ -10,3 +10,12 @@ output "vpc_subnet_zone" {
 output "vpc_subnet_v4_cidr_blocks" {
   value = yandex_vpc_subnet.devops.v4_cidr_blocks
 }
+
+output "vm_details" {
+  value = [
+    for instance in yandex_compute_instance.ansible : {
+      name        = instance.name,
+      ip_address  = instance.network_interface[0].nat_ip_address
+    }
+  ]
+}
